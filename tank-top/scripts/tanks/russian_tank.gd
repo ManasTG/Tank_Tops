51e5f5bd383d0f39c4ae289c83c808e3db9010f9
Manas Madhukar <manas.jilal@gmail.com>
2026-01-07 23:34:20 +0530

    7-1-25 [2]
    Working tank base: movement

--- tank-top/scripts/tanks/russian_tank.gd
+++ tank-top/scripts/tanks/russian_tank.gd
@@ -4,25 +4,23 @@ extends CharacterBody2D
 
 
 
-const max_speed: int = 100
-const acceleration: int = 20
-const friction: int = 8
-const rotation_speed: int = 180	
+const max_speed: float = 100.0
+const acceleration: float = 20.0
+const friction: float = 8.0
+const rotation_speed: float = 180.0
 
 func _physics_process(delta: float) -> void:
-	
+
 	#Go forward and backwars
 	var move_input := Input.get_action_strength("forward") - Input.get_action_strength("backward")
 	
 	#input right and left then convert it into rotiation form in rotation_degrees
-	var turn_input := Input.get_action_strength("right") - Input.get_action_strength("left")
-	if move_input == -1 :# using < 0 might be better
-		turn_input = Input.get_action_strength("left") - Input.get_action_strength("right")
-	
-	rotation_degrees += turn_input * rotation_speed * delta
-	
+	var turn_input := 0
+	#if abs(move_input) > 0.1:
+		#turn_input = Input.get_action_strength("right") - Input.get_action_strength("left")
 
-	
+	rotation_degrees += turn_input * rotation_speed * delta
+
 	#direction in which the tank is facing.. which is for the start up but can be rotated with rotation variable using rotated function.
 	var direction := Vector2.UP.rotated(rotation)
 	
